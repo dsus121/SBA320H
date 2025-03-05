b872be185b23fd5649013966ae8a3db171294e66
Dawn Cheairs <dawnsusanne@gmail.com>
2025-03-04 22:04:51 -0700

    Removed the gigantic console.log

--- src/hooks/useCharities.js
+++ src/hooks/useCharities.js
@@ -26,13 +26,14 @@ const useCharities = () => {
         const response = await fetch(
           "https://data.colorado.gov/resource/7jm9-f28m.json?$query=SELECT%0A%20%20%60entityid%60%2C%0A%20%20%60documentid%60%2C%0A%20%20%60fein%60%2C%0A%20%20%60name%60%2C%0A%20%20%60statementofpurpose%60%2C%0A%20%20%60filingdate%60%0AWHERE%0A%20%20caseless_contains(%60statementofpurpose%60%2C%20%22animals%22)%0A%20%20%20%20OR%20(caseless_contains(%60statementofpurpose%60%2C%20%22environment%22)%0A%20%20%20%20%20%20%20%20%20%20OR%20(caseless_contains(%60statementofpurpose%60%2C%20%22environmental%22)%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20OR%20contains(%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%60statementofpurpose%60%2C%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%22social%20responsibility%22%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20)))"
         );
+if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
         const data = await response.json();
         dispatch({ type: "FETCH_SUCCESS", payload: data });
-        console.log(data);
-      } catch (error) {
+              } catch (error) {
         dispatch({ type: "FETCH_ERROR", payload: error.message });
-        console.error("Error fetching charity data:", error);
-      }
+              }
     };
 
     fetchCharities();
